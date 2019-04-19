787c552e49b304a805363453d9b689c141699816
Florian <florian@stampix.com>
2019-04-19 12:12:29 +0200

    create flexbox for countdown elements

--- script/countdown.js
+++ script/countdown.js
@@ -16,7 +16,10 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Output the result in an element with id="countdown"
-  document.getElementById('countdown').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+  document.getElementById('countdown').innerHTML = `<div id="days" class="countdown-item">${days}d</div>
+  <div id="hours" class="countdown-item">${hours}h</div>
+  <div id="minutes" class="countdown-item">${minutes}m</div>
+  <div id="seconds" class="countdown-item">${seconds}s</div>`
 
   // If the count down is over, write some text
   if (distance < 0) {
